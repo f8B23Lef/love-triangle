349d8b26494fc54f74bb3f247a3e561f958b3a8b
f8B23Lef <mal7rish@yandex.ru>
2018-09-14 14:13:08 +0300

    Implement calculates love triangles function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,54 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  	var loveTrianglesArr = [];
+
+    for(var i = 0; i < preferences.length; i++) {
+
+		var a = preferences[i];
+		if(a > preferences.length) continue;
+
+		var b = preferences[a-1];
+		if(b > preferences.length) continue;
+
+		var c = preferences[b-1];
+		if(c > preferences.length) continue;
+
+		var isTriangle = (a == preferences[c-1] && a != b && a != c && b != c);
+
+		if(isTriangle) {
+
+			var curLoveTriangleArr = [a, b, c].sort(function(a, b) {
+				return a - b
+			});
+	
+			if(loveTrianglesArr.length == 0) {
+				loveTrianglesArr.push(curLoveTriangleArr);
+			} else {
+				if(!containsArray(loveTrianglesArr, curLoveTriangleArr)) {
+					loveTrianglesArr.push(curLoveTriangleArr);
+				}
+			}	
+		}
+	}
+  return loveTrianglesArr.length;
 };
+
+function containsArray(bigArr, targetArr) {
+
+	for(var i = 0; i < bigArr.length; i++) {
+		var equal = true;
+		for(var j = 0; j < targetArr.length; j++) {
+			if (bigArr[i][j] != targetArr[j]) {
+				equal = false;
+				break;
+			}
+		}
+		
+		if(equal) {
+			return true;
+		}
+	}
+	return false;
+}
